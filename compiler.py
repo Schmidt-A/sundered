3250b90760bc65c4b602fab82092f52d60a00293
Schmidt-A <caffeinated.tweek@gmail.com>
2016-11-03 14:10:06 -0500

    Added ability to compile all scripts

--- compiler.py
+++ compiler.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import sys
 
@@ -16,18 +17,20 @@ def help():
     sys.exit()
 
 
-def compile(nss_file):
+def compile_nss(nss_file):
     if not nss_file.startswith(SCRIPT_DIR):
         nss_file = SCRIPT_DIR + nss_file
 
     # ./tools/nwnnsscomp -i scripts/bioware_scripts/ -c scripts/mod_playerenter.nss
-    print 'Compiling nss file ' + nss_file + "..."
+    print '\nCompiling nss file ' + nss_file + "..."
     cmd = COMPILER + ' -i ' + BIOWARE_SCRIPTS + ' -c ' + nss_file
     print subprocess.Popen(cmd, shell=True,
             stdout=subprocess.PIPE).stdout.read()
     print '... done'
 
-    print 'Removing generated ncs files...'
+
+def clean():
+    print '\nRemoving generated ncs files...'
     cmd = 'rm ' + SCRIPT_DIR + '*ncs'
     print subprocess.Popen(cmd, shell=True,
             stdout=subprocess.PIPE).stdout.read()
@@ -38,9 +41,13 @@ def main(args):
     if len(args) == 0 or args[0] == '-h':
         help()
     elif args[0] == 'all':
-        pass #TODO: compile all scripts
+        for fname in os.listdir(SCRIPT_DIR):
+            if fname.endswith('.nss'):
+                compile_nss(fname)
+        clean()
     else:
-        compile(args[0])
+        compile_nss(args[0])
+        clean();
 
 
 if __name__ == '__main__':
